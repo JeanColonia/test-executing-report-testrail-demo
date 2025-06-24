3e9c04744d1511e0576ba649b51ea757b741ab72
Jean Colonia <engshka123@gmail.com>
2025-06-24 16:40:17 -0500

    devtools version

--- src/main/java/org/nitro_qa/util/PdfMerger.java
+++ src/main/java/org/nitro_qa/util/PdfMerger.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.devtools.DevTools;
-import org.openqa.selenium.devtools.v137.page.Page;
+import org.openqa.selenium.devtools.v123.page.Page;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Service;
